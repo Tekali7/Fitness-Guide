8627465d84a899469cd3106574a5a731906c58e3
Tekali7 <alihan.teker@hotmail.com>
2024-01-01 16:24:04 +0100

    Create main function

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def main():
+    """
+    Run all program functions.
+    """
+    print("Welcome to the Fitness Guide!\n")
+
+    while True:
+        user_selection = input("Do you want to use the Strength Guide or the BMI Calculator? (Type 'strength' or 'bmi'):\n").lower()
+
+        if user_selection == "strength":
+            strength_guide()
+            break
+        elif user_selection == "bmi":
+            bmi_calculator()
+            break
+        else:
+            print("Invalid input. Enter 'strength' or 'bmi'.\n")
+
+main()
