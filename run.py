34b2128fbaafb92ffb2c79199c3fa7f09632dcd4
Tekali7 <alihan.teker@hotmail.com>
2024-01-01 17:22:32 +0100

    Create strength_guide and bmi_calculator function

--- run.py
+++ run.py
@@ -1,12 +1,40 @@
+def strength_guide():
+    """
+    Show the average strength of other lifters of the same gender, weight and age.
+    """
+
+    print("Welcome to the Strength Guide!\n")
+
+    gender = str(input("Enter your gender ('m' or 'f'): \n")).lower()
+    weight = int(input("Enter your weight in kg: \n"))
+    age = int(input("Enter your age: \n"))
+
+
+
+def bmi_calculator():
+    """
+    Calculate the BMI of the user with their gender, weight, height and age.
+    [weight (kg) / height (cm) / height (cm)] x 10,000
+    """
+
+    print("Welcome to the BMI Calculator!\n")
+
+    gender = str(input("Enter your gender ('m' or 'f'): \n")).lower()
+    weight = int(input("Enter your weight in kg: \n"))
+    height = int(input("Enter your height in cm: \n"))
+    age = int(input("Enter your age: \n"))
+
+
+
 
 def main():
     """
-    Run all program functions.
+    Run program functions.
     """
     print("Welcome to the Fitness Guide!\n")
 
     while True:
-        user_selection = input("Do you want to use the Strength Guide or the BMI Calculator? (Type 'strength' or 'bmi'):\n").lower()
+        user_selection = input("Do you want to use the Strength Guide or the BMI Calculator? (Type 'strength' or 'bmi'): \n").lower()
 
         if user_selection == "strength":
             strength_guide()
