da298941e986cc202778bac83f32bdde9d25dd11
Tekali7 <alihan.teker@hotmail.com>
2024-01-04 10:01:55 +0100

    Add docstrings to functions

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
 def strength_guide():
     """
-    Show the average strength of other lifters of the same gender, weight and age.
+    Show the average strength of other lifters
+    of the same gender, weight and age.
+
+    Arguments:
+    None, user inputs for gender, weight
+    and exercise are obtained interactively
+    from the users input.
+
+    Returns:
+    None, displays average strength
+    references and prompts the user for further actions.
     """
     while True:
         print("Welcome to the Strength Guide!\n")
@@ -25,28 +35,29 @@ def strength_guide():
 
         # Exercise input validation "squat", "bench" or "deadlift"
         exercise = input("Choose an exercise ('squat', 'bench' or 'deadlift'): \n").lower().strip()
-        
+
         while exercise not in ["bench", "squat", "deadlift"]:
             print("Invalid input. Enter an exercise ('bench', 'squat' or 'deadlift'): \n")
             exercise = input("Choose an exercise ('squat', 'bench' or 'deadlift'): \n").lower().strip()
 
         exercise_mapping = get_exercise_mapping(gender, weight)
-        
+
         print(f"\nAverage strength reference for a {gender} lifter with {weight}kg bodyweight:")
 
         if weight >= 50 and weight <= 80:
             print(f"{exercise_mapping[exercise]} kg for {exercise}.\n")
         elif weight > 80 and weight <= 120:
             print(f"{exercise_mapping[exercise]} kg for {exercise}.\n")
-        else: 
+        else:
             print(f"{exercise_mapping[exercise]} kg for {exercise}.\n")
 
-    # Ask the user if they want to get another strength reference, use the bmi calculator, or exit
+    # Ask the user if they want to get another strength reference,
+    # use the bmi calculator, or exit
         user_choice = input("Do you want to:\n"
-                        "1. Get another Strength Reference\n"
-                        "2. Use the BMI Calculator\n"
-                        "3. Exit\n"
-                        "Enter the number corresponding to your choice: ")
+                    "1. Get another Strength Reference\n"
+                    "2. Use the BMI Calculator\n"
+                    "3. Exit\n"
+                    "Enter the number corresponding to your choice: ")
 
         if user_choice == '1':
             # Continue with another strength reference
@@ -62,14 +73,19 @@ def strength_guide():
         else:
             print("Invalid choice. Please enter '1', '2', or '3'.\n")
 
+
 def get_exercise_mapping(gender, weight):
     """
-    Arguments: 
-        gender: str - One of 'm' or 'f'.
-        weight: int - Weight in kg. Must be between 50 and 140.
+    Retrieves average strength exercise mapping
+    based on the users gender and weight.
+
+    Arguments:
+        gender: str - Users gender, either 'm' or 'f'.
+        weight: int - Users weight in kg, must be between 50 and 140.
 
     Returns:
-        dict of str -> int
+        dict: A dictionary representing exercise
+        mapping for the users gender and weight.
     """
 
     exercise_mapping = {
@@ -112,29 +128,36 @@ def get_exercise_mapping(gender, weight):
     if gender == "m":
         if weight >= 50 and weight <= 80:
             return exercise_mapping["m"]["light"]
-        
+
         elif weight > 80 and weight <= 120:
             return exercise_mapping["m"]["middle"]
-        
-        else: 
+
+        else:
             weight > 120 and weight <= 140
             return exercise_mapping["m"]["heavy"]
-    
+
     if gender == "f":
         if weight >= 50 and weight <= 100:
             return exercise_mapping["f"]["light"]
-        
+
         elif weight > 100 and weight <= 120:
             return exercise_mapping["f"]["middle"]
-        
-        else: 
+
+        else:
             weight > 120 and weight <= 140
             return exercise_mapping["f"]["heavy"]
 
 def bmi_calculator():
     """
-    Calculate the BMI of the user with their gender, weight, height and age.
-    [weight (kg) / height (cm) / height (cm)] x 10000
+    Calculates the BMI of the user with their weight and height,
+    and provides options for further actions.
+
+    Arguments:
+    None
+
+    Returns:
+    None, displays calculated BMI
+    and prompts the user for further actions.
     """
     while True:
         print("Welcome to the BMI Calculator!\n")
@@ -159,7 +182,7 @@ def bmi_calculator():
                 else:
                     print("Invalid input. Enter a value between 150 and 200.\n")
             except ValueError:
-                    print("Invalid input. Enter a valid integer for height.\n")
+                print("Invalid input. Enter a valid integer for height.\n")
         
         # Calculate the BMI
         bmi = calculate_bmi(weight, height)
@@ -190,13 +213,33 @@ def bmi_calculator():
 
 def calculate_bmi(weight_kg, height_cm):
     """
-    ADD DOCSTRING HEREEE
+    Calculates the BMI based on
+    the users weight and height.
+    
+
+    Arguments:
+        weight_kg (int): Users weight in kilograms.
+        height_cm (int): Users height in centimeters.
+
+    Returns:
+        float: Calculated BMI using the formula
+        [weight (kg) / height (cm) / height (cm)] x 10000.
     """
     return (weight_kg / height_cm ** 2) * 10000
 
 def main():
     """
-    Run program functions.
+    Welcome the user to the Fitness Guide and
+    prompt them to choose between the Strength Guide
+    or the BMI Calculator. Continues to execute the
+    selected functionality until the user exits.
+
+    Arguments:
+    None
+
+    Returns:
+    None, prints outputs and guides the user
+    through the selected program functions.
     """
     print("Welcome to the Fitness Guide!\n")
 
@@ -212,5 +255,6 @@ def main():
         else:
             print("Invalid input. Enter 'strength' or 'bmi'.\n")
 
+# Start point of the script, calls the main function.
 if __name__ == "__main__":
     main()
